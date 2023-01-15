b216e6334cf5f6903cad72a6cb41faac0388a085
minhqtran <kunnietran159@gmail.com>
2023-01-15 17:47:39 +0700

    Finish Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(arr, value) {
+  if(value < 0) return "ERROR";
+  let str = '';
+  for (let i = 0; i < value; i++){
+    str += arr;
+  }
+  
+  return str;
 };
+
+console.log(repeatString("sldk", -1));
+
 
 // Do not edit below this line
 module.exports = repeatString;
