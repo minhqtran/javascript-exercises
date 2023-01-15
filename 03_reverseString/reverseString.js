545ef2c56d0719e9bfbe8458884db7586f04bd21
minhqtran <kunnietran159@gmail.com>
2023-01-15 18:01:06 +0700

    Finish Excercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+  return str.split('').reverse().join('')
 };
+
+console.log(reverseString('concac'))
 
 // Do not edit below this line
 module.exports = reverseString;
